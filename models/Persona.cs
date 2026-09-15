71410fa72a2e0c4cf4d29394a82a77f90cbfdeb2
Dona <beharrysantoyo@gmail.com>
2026-09-15 09:16:59 -0500

    feat : init model (DTO) Persona

--- models/Persona.cs
+++ models/Persona.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace practica_form_duna.models
 {
+		/// <summary>
+		///  Modelo DTO de la tabla persona
+		/// </summary>
 		internal class Persona
 		{
+				public long? id { get; private set; }
+				public string nombre { get; private set; }
+				public string telefono { get; private set; }
+				//cadena de conexion de mysql
+				
+
+				public Persona(long? id, string nombre, string telefono)
+				{
+						this.id = id ?? 0;
+						this.nombre = nombre;
+						this.telefono = telefono;
+				}
+
+				// IMPORTANTE: Aqui va todo el sql 
+				private readonly string cadenaConexion = @"";
+
 		}
 }
