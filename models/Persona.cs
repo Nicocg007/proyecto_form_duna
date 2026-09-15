c1be99792b5d0b1a3886554472f452529afaf818
Nico_CG <niconcg007@gmail.com>
2026-09-15 10:25:50 -0500

    conexion a base de datos

--- models/Persona.cs
+++ models/Persona.cs
@@ -1,31 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace practica_form_duna.models
 {
-		/// <summary>
-		///  Modelo DTO de la tabla persona
-		/// </summary>
-		internal class Persona
-		{
-				public long? id { get; private set; }
-				public string nombre { get; private set; }
-				public string telefono { get; private set; }
-				//cadena de conexion de mysql
-				
+    /// <summary>
+    ///  Modelo DTO de la tabla persona
+    /// </summary>
+    internal class Persona
+    {
+        public long? id { get; private set; }
+        public string nombre { get; private set; }
+        public string telefono { get; private set; }
+        //cadena de conexion de mysql
 
-				public Persona(long? id, string nombre, string telefono)
-				{
-						this.id = id ?? 0;
-						this.nombre = nombre;
-						this.telefono = telefono;
-				}
+        public Persona(long? id, string nombre, string telefono)
+        {
+            this.id = id ?? 0;
+            this.nombre = nombre;
+            this.telefono = telefono;
+        }
 
-				// IMPORTANTE: Aqui va todo el sql 
-				private readonly string cadenaConexion = @"";
+        // IMPORTANTE: Aqui va todo el sql
+        private static readonly string cadenaConexion = @"Server=localhost\SQLEXPRESS;Database=db_practica;Trusted_Connection=True;TrustServerCertificate=True;";
 
-		}
+        public static List<Persona> getAll()
+        {
+            List<Persona> listaPersonas = new List<Persona>();
+            string query = "SELECT id, nombre, telefono FROM Persona";
+
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+
+                    using (SqlCommand comando = new SqlCommand(query, conexion))
+                    {
+                        using (SqlDataReader reader = comando.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Mapeamos cada fila a un objeto Persona
+                                Persona persona = new Persona(
+                                    Convert.ToInt64(reader["id"]),
+                                    reader["nombre"].ToString(),
+                                    reader["telefono"].ToString()
+                                );
+
+                                listaPersonas.Add(persona);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al obtener los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            // Ahora sí devolvemos la lista correctamente
+            return listaPersonas;
+        }
+    }
 }
--- views/Form1.Designer.cs
+++ views/Form1.Designer.cs
@@ -28,94 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-						this.dataGridView1 = new System.Windows.Forms.DataGridView();
-						this.btnAgregar = new System.Windows.Forms.Button();
-						this.btnEditar = new System.Windows.Forms.Button();
-						this.btnEliminar = new System.Windows.Forms.Button();
-						this.txtBuscar = new System.Windows.Forms.TextBox();
-						this.btnBuscar = new System.Windows.Forms.Button();
-						((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-						this.SuspendLayout();
-						// 
-						// dataGridView1
-						// 
-						this.dataGridView1.AllowUserToAddRows = false;
-						this.dataGridView1.AllowUserToDeleteRows = false;
-						this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-						this.dataGridView1.Location = new System.Drawing.Point(16, 133);
-						this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-						this.dataGridView1.Name = "dataGridView1";
-						this.dataGridView1.ReadOnly = true;
-						this.dataGridView1.Size = new System.Drawing.Size(815, 474);
-						this.dataGridView1.TabIndex = 0;
-						// 
-						// btnAgregar
-						// 
-						this.btnAgregar.Location = new System.Drawing.Point(839, 133);
-						this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-						this.btnAgregar.Name = "btnAgregar";
-						this.btnAgregar.Size = new System.Drawing.Size(127, 60);
-						this.btnAgregar.TabIndex = 1;
-						this.btnAgregar.Text = "Agregar";
-						this.btnAgregar.UseVisualStyleBackColor = true;
-						// 
-						// btnEditar
-						// 
-						this.btnEditar.Location = new System.Drawing.Point(839, 243);
-						this.btnEditar.Margin = new System.Windows.Forms.Padding(4);
-						this.btnEditar.Name = "btnEditar";
-						this.btnEditar.Size = new System.Drawing.Size(127, 60);
-						this.btnEditar.TabIndex = 2;
-						this.btnEditar.Text = "Editar";
-						this.btnEditar.UseVisualStyleBackColor = true;
-						// 
-						// btnEliminar
-						// 
-						this.btnEliminar.Location = new System.Drawing.Point(839, 340);
-						this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
-						this.btnEliminar.Name = "btnEliminar";
-						this.btnEliminar.Size = new System.Drawing.Size(127, 60);
-						this.btnEliminar.TabIndex = 3;
-						this.btnEliminar.Text = "Eliminar";
-						this.btnEliminar.UseVisualStyleBackColor = true;
-						// 
-						// txtBuscar
-						// 
-						this.txtBuscar.Location = new System.Drawing.Point(16, 83);
-						this.txtBuscar.Name = "txtBuscar";
-						this.txtBuscar.Size = new System.Drawing.Size(310, 25);
-						this.txtBuscar.TabIndex = 4;
-						this.txtBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-						// 
-						// btnBuscar
-						// 
-						this.btnBuscar.Location = new System.Drawing.Point(333, 79);
-						this.btnBuscar.Margin = new System.Windows.Forms.Padding(4);
-						this.btnBuscar.Name = "btnBuscar";
-						this.btnBuscar.Size = new System.Drawing.Size(127, 33);
-						this.btnBuscar.TabIndex = 5;
-						this.btnBuscar.Text = "Buscar";
-						this.btnBuscar.UseVisualStyleBackColor = true;
-						// 
-						// Form1
-						// 
-						this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
-						this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-						this.ClientSize = new System.Drawing.Size(1012, 623);
-						this.Controls.Add(this.btnBuscar);
-						this.Controls.Add(this.txtBuscar);
-						this.Controls.Add(this.btnEliminar);
-						this.Controls.Add(this.btnEditar);
-						this.Controls.Add(this.btnAgregar);
-						this.Controls.Add(this.dataGridView1);
-						this.Font = new System.Drawing.Font("Noto Sans Lisu", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-						this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-						this.Name = "Form1";
-						this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-						this.Text = "Form1";
-						((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-						this.ResumeLayout(false);
-						this.PerformLayout();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 133);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(815, 474);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(839, 133);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(127, 60);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(839, 243);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(127, 60);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(839, 340);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(127, 60);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(16, 83);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(310, 26);
+            this.txtBuscar.TabIndex = 4;
+            this.txtBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(333, 79);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(127, 33);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 623);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
